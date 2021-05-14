b1d85e8dd0d04bad42562b6f5e019b62fea88130
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-05-13 23:42:56 -0500

    update root .gitignore

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is a mono-repo for Notion Integrations
+
+
---
+++ upload-md-to-notion/index.js
@@ -0,0 +1,7 @@
+const {Client} require("@notionhq/client")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const notion = new Client({auth: process.env.NOTION_KEY })
+
